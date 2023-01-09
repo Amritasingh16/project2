841edd8b506adb7f97e3d102289139476b8f4ec5
bhavi321 <bhavialkhaniya5@gmail.com>
2023-01-09 19:57:22 +0530

    a

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -13,8 +13,9 @@ const InternModel= require("../models/intern")
  const getCollegeDetails= async function(req,res){
     try{
     let data = req.query.collegeName;
+    
     // data.isDeleted=false;
-    let findByName=await CollegeModel.findOne({name:data})
+    let findByName=await CollegeModel.findOne({name:data,isDeleted:false})
     findByName=findByName.toObject();
     if(!findByName){
         return res.status(404).send({status:false,msg:"college not found"})
@@ -22,10 +23,17 @@ const InternModel= require("../models/intern")
     console.log(findByName)
 
     
-let findData = await InternModel.find({ collegeId: findByName._id });
+let findData = await InternModel.find({ collegeId: findByName._id }).select({collegeId:0,isDeleted:0,createdAt:0,updatedAt:0,__v:0})
+if(findData.length==0) return res.status(404).send({status:false,msg:"no intern found"})
     console.log(findData)
-    findByName.interns=findData
-    res.status(200).send({status:true,data:findByName})
+     findByName.interns=findData
+    const sendData={
+        name:findByName.name,
+        fullName:findByName.fullName,
+        logoLink:findByName.logoLink,
+        intern:findData
+    }
+    res.status(200).send({status:true,data:sendData})
     }
     catch(error){
     res.status(500).send({status: false, error: error.message})
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -3,12 +3,16 @@ const CollegeModel= require("../models/college")
 const createIntern= async function(req,res){
   try{  
     let data= req.body
-    let collegeId= await CollegeModel.findOne({name:data.collegeName})
+    let collegeId= await CollegeModel.findOne({name:data.collegeName,isDeleted:false})
     // console.log(getData)
     data.collegeId=collegeId._id
     let created= await InternModel.create(data)
     
-    res.status(201).send({ status: true, data: created})
+    res.status(201).send({ status: true, data:{isDeleted:created.isDeleted,
+    name:created.name,
+    email:created.email,
+    mobile:created.mobile,
+    collegeId:created.collegeId}})
   }catch(error){
     res.status(500).send({ status: false, error: error.message })
   }
