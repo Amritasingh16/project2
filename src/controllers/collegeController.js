3a957ae3346dc8a50e7394133d6e7fcbc708f8d9
Amritasingh16 <aamritassingh1@gmail.com>
2023-01-09 22:37:26 +0530

    validations

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,46 +1,75 @@
-const CollegeModel= require("../models/college")
-const InternModel= require("../models/intern")
- const createCollege=  async function(req,res){
-  try{  
-    let data= req.body
-    let created= await CollegeModel.create(data)
-    res.status(201).send({status: true, data: created})
-  }catch(error){
-    res.status(500).send({status: false, error: error.message })
+const CollegeModel = require("../models/college")
+const InternModel = require("../models/intern")
+
+
+//==============================CREATING COLLEGE===============================
+
+const createCollege = async function (req, res) {
+  try {
+    let data = req.body
+    if (Object.keys(data).length == 0) {
+      return res
+          .status(400)
+          .send({ status: false, message: "All Keys are Mandatory" });
   }
- }
 
- const getCollegeDetails= async function(req,res){
-    try{
+  const { name, fullName, logoLink } = data;
+
+  if (!name) {
+      return res.status(400).send({ status: false, msg: "Name is required" });
+  }
+
+  if (!fullName) {
+      return res.status(400).send({ status: false, msg: "Fullname is required" });
+  }
+
+  // if (!logoLink) {
+  //     return res
+  //         .status(400)
+  //         .send({ status: false, msg: "LogoLink is not a valid " });
+  // }
+
+  
+  
+    let created = await CollegeModel.create(data)
+    res.status(201).send({ status: true, data: created })
+  } catch (error) {
+    res.status(500).send({ status: false, error: error.message })
+  }
+}
+
+
+//======================GETTING COLLEGE DETAILS====================================
+
+const getCollegeDetails = async function (req, res) {
+  try {
     let data = req.query.collegeName;
-    
-    // data.isDeleted=false;
-    let findByName=await CollegeModel.findOne({name:data,isDeleted:false})
-    findByName=findByName.toObject();
-    if(!findByName){
-        return res.status(404).send({status:false,msg:"college not found"})
-    }
-    console.log(findByName)
+    if(!data) return res.status(404).send({status: false, ERROR: "Data not found"})
 
-    
-let findData = await InternModel.find({ collegeId: findByName._id }).select({collegeId:0,isDeleted:0,createdAt:0,updatedAt:0,__v:0})
-if(findData.length==0) return res.status(404).send({status:false,msg:"no intern found"})
-    console.log(findData)
-     findByName.interns=findData
-    const sendData={
-        name:findByName.name,
-        fullName:findByName.fullName,
-        logoLink:findByName.logoLink,
-        intern:findData
+    let findByName = await CollegeModel.findOne({ name: data, isDeleted: false })
+
+   
+    if (!findByName) {
+
+      return res.status(404).send({ status: false, msg: "College not found" })
     }
-    res.status(200).send({status:true,data:sendData})
-    }
-    catch(error){
-    res.status(500).send({status: false, error: error.message})
-    }
- }
+     let findData = await InternModel.find({ collegeId: findByName._id }).select({ collegeId: 0, isDeleted: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+    if (findData.length == 0) return res.status(404).send({ status: false, msg: "no intern found" })
+   
+    const sendData = {
+              name: findByName.name,
+              fullName: findByName.fullName,
+              logoLink: findByName.logoLink,
+              intern: findData
+            }
+            res.status(200).send({ status: true, data: sendData })
+  }
+  catch (error) {
+    res.status(500).send({ status: false, error: error.message })
+  }
+}
 
 
 
- module.exports.createCollege=createCollege
- module.exports.getCollegeDetails=getCollegeDetails
+module.exports.createCollege = createCollege
+module.exports.getCollegeDetails = getCollegeDetails
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,18 +1,29 @@
 const InternModel= require("../models/intern")
 const CollegeModel= require("../models/college")
+
+//=================================CREATE INTERNS================================
 const createIntern= async function(req,res){
   try{  
     let data= req.body
+
+    if(data.length==0) return res.status(404).send({status:false, ERROR: "Data required"})
+
     let collegeId= await CollegeModel.findOne({name:data.collegeName,isDeleted:false})
-    // console.log(getData)
-    data.collegeId=collegeId._id
-    let created= await InternModel.create(data)
+      if(!collegeId){
+       return res.status(400).send({status: false, ERROR: "College not Found"})
+     }
+        data.collegeId=collegeId._id
+        let created= await InternModel.create(data)
+        if(!created) {
+         return res.send(400).send({status: false, ERROR: "Enter valid details"})
+        }else{
     
-    res.status(201).send({ status: true, data:{isDeleted:created.isDeleted,
-    name:created.name,
-    email:created.email,
-    mobile:created.mobile,
-    collegeId:created.collegeId}})
+        res.status(201).send({ status: true, data:{isDeleted:created.isDeleted,
+       name:created.name,
+       email:created.email,
+       mobile:created.mobile,
+       collegeId:created.collegeId}})
+    }
   }catch(error){
     res.status(500).send({ status: false, error: error.message })
   }
